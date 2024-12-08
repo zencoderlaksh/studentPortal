b8fff0e7848dc13ddd99e04deaf64520eb5273a4
Coder Laksh <zencoderlaksh@gmail.com>
2024-12-08 11:16:18 +0530

    first commit

---
+++ Frontend/src/services/authService.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const authService = () => {
+  return <div>authService</div>;
+};
+
+export default authService;
---
+++ Frontend/src/services/courseService.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const courseService = () => {
+  return <div>courseService</div>;
+};
+
+export default courseService;
